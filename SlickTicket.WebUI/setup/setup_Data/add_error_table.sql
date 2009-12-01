c1ba8435ffb86131290722c278f5f4a321bd21bc
naspinski_cp <SND\naspinski_cp>
2009-12-01 11:08:13 +0000

    setup/setup_Data/add_error_table.sql is the sql to add the new errors table into the DB

---
+++ SlickTicket.WebUI/setup/setup_Data/add_error_table.sql
@@ -0,0 +1,13 @@
+﻿USE [SlickTicket]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[errors](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[title] [nvarchar](100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	[details] [nvarchar](max) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	[occured] [datetime] NOT NULL
+) ON [PRIMARY]
