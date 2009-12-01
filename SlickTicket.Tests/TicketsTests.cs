77b54091599074a7a8d5a14272e83679ebd02b07
naspinski_cp <SND\naspinski_cp>
2009-12-01 09:23:26 +0000

    restructuring to add more stuff

---
+++ SlickTicket.Tests/TicketsTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using SlickTicket.Tests;
+
+namespace SlickTicket.Tests
+{
+    [TestFixture]
+    public class TicketsTests
+    {
+        [SetUp]
+        public void Setup()
+        { }
+
+        [Test]
+        public void New_ticket_error_fails()
+        {
+        }
+    }
+}
