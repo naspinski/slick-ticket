dfb10a46e45e4c36b29783ca316201b285fc0de1
naspinski_cp <SND\naspinski_cp>
2009-12-01 12:31:43 +0000

    Starting to add in API for email interaction in the SlickTicket.DomainModel project

---
+++ SlickTicket.DomainModel/Errors.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SlickTicket.DomainModel
+{
+    public class Errors
+    {
+        private static string N = Environment.NewLine;
+
+        public static void New(dbDataContext db, string title_prefix, Exception exception)
+        {
+            error e = new error() { title = title_prefix + " - " + exception.Message, occured = DateTime.Now };
+            e.details = "Exception.Data:" + N;
+            foreach(var key in exception.Data.Keys)
+                    e.details += key.ToString() + ": " + exception.Data[key].ToString() + N;
+            db.errors.InsertOnSubmit(e);
+            db.SubmitChanges();
+        }
+    }
+}
---
+++ SlickTicket.DomainModel/Units.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SlickTicket.DomainModel
+{
+    public class Units
+    {
+        public static int Default //defaults to the lowest permission subgroup
+        { get { return new dbDataContext().sub_units.OrderBy(x => x.access_level).First().id; } }
+    }
+}
